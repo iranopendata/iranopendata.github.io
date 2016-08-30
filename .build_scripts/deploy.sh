4fb1c54bfa8725ce66d5598a58039ba49d4817d7
alifelski <ali@developmentseed.org>
2016-08-30 17:15:49 -0400

    travis set up

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -2,11 +2,11 @@
 set -e # halt script on error
 
 echo "Get ready, we're pushing to gh-pages!"
-cd _site
+cd app
 git init
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
 git add .
-git commit -m "CI deploy to gh-pages"
+git commit -m "CI deploy to master"
 git show-ref
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:master
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" develop:master
