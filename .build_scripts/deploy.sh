71f85deae11960557ed17b11d9d3609f0bbe036a
alifelski <ali@developmentseed.org>
2016-08-30 17:26:09 -0400

    Fix refspec master

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -9,4 +9,4 @@ git config user.email "travis@somewhere.com"
 git add .
 git commit -m "CI deploy to master"
 git show-ref
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" develop:master
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:master
