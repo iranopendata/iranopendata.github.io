c23d26190bdbc6f433e70aaad16d5cb43fac3ded
Marc Farra <marcfarra@gmail.com>
2016-09-14 17:45:28 -0400

    Sticky nav class
    * This commit highlights the nav element denoting the current page being
    rendered. This is done by adding an id to each page and using the id in
    liquid templates.
    
    * Also reverts changes in previous commit since JS can't be used for
      this feature.

--- app/assets/scripts/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".data").on('click', function(){
-    	console.log("hello i'm clicked");
-    	$(".nav-list_item").removeClass("active");
-        $(".data").addClass("active");
-    });
-});
