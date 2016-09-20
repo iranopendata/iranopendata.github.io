95ebacff90f10ff07de009e9fa9e305b4063228d
Marc Farra <marcfarra@gmail.com>
2016-09-19 21:24:51 -0400

    Initial featured datasets work

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -166,6 +166,78 @@ class Dataset extends Component {
   }
 }
 
+class FeaturedList extends Component {
+  constructor () {
+    super();
+
+    this.APIUrl = '/catalog/index.json';
+    if (process.env.NODE_ENV == 'development') {
+      this.APIUrl = 'http://localhost:8000/index.json';
+    }
+  }
+
+  componentWillMount () {
+    const {featured1, featured2} = featured;
+    const component = this;
+
+    fetch(component.APIUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        return json.datasets.map(transformDatasetFromIndex);
+      })
+      .then(function (datasets) {
+        console.log(datasets);
+        const filtered = datasets.filter(function (dataset) {
+          return dataset.name === featured1 || dataset.name === featured2;
+        });
+        console.log(filtered);
+        if (filtered.length == 2) {
+          component.setState({
+            featured1: filtered[0],
+            featured2: filtered[1]
+          });
+        }
+      });
+  }
+
+  render ({}, {featured1, featured2}) {
+    if (featured1 && featured2) {
+      console.log(featured1);
+      return h(
+        'ul', {class: 'list-type-none'},
+        h('li', {},
+          h('span', {class: 'type-category'}, featured1.category),
+          h('h5', {class: 'header-with-description'},
+            h('a', {class: 'text-link', 'href': `/${PAGE_LANG}/datasets/${featured1.name}`}, featured1.title)
+           ),
+          h('dl', {class: 'metadata'},
+            h('dt', {class: 'metadata-title'}, lang['featured-source']),
+            ' ',
+            h('dd', {class: 'metadata-description'}, featured1.source)
+           ),
+          h('p', { }, featured1.description.slice(0, 140) + '...')
+         ),
+
+        h('li', {},
+          h('span', {class: 'type-category'}, featured2.category),
+          h('h5', {class: 'header-with-description'},
+            h('a', {class: 'text-link', 'href': `/${PAGE_LANG}/datasets/${featured2.name}`}, featured2.title)
+           ),
+          h('dl', {class: 'metadata'},
+            h('dt', {class: 'metadata-title'}, lang['featured-source']),
+            '  ',
+            h('dd', {class: 'metadata-description'}, featured2.source)
+           ),
+          h('p', { }, featured2.description.slice(0, 140) + '...')
+         )
+      );
+    }
+    return h('div');
+  }
+}
+
 class DatasetList extends Component {
 
   constructor () {
@@ -456,6 +528,11 @@ if (content) {
 const dataset = document.getElementById('wrapper-content-dataset');
 if (dataset) {
   render(h(Dataset), dataset);
+}
+
+const featuredList = document.getElementById('featured-homepage');
+if (featuredList) {
+  render(h(FeaturedList), featuredList);
 }
 
 if (document.readyState != 'loading'){
