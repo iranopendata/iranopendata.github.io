70e70e940aaed53418f6d687d80645d213326e37
Marc Farra <marcfarra@gmail.com>
2016-09-21 19:09:17 -0400

    Add 404 page
    Add 404 page that redirects to index.html with query parameter
    containing the requested resource. The URL is then changed
    back (without requesting a new page). This allows us to render react
    components at clean URLs for datasets that don't have a corresponding
    notes pages.

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,4 +1,7 @@
 import {h, render, Component} from 'preact';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
 
 import Dataset from './components/Dataset';
 import DatasetList from './components/DatasetList';
@@ -40,6 +43,43 @@ function reviseDataFilter () {
 }
 
 function onReady () {
+  const page_content = document.getElementById('page-content');
+  const unlisten = history.listen((location, action) => {
+
+    console.log(action, location.pathname, location.state);
+    switch (location.pathname) {
+    case '/404.html':
+      page_content.innerHTML = '<p style="margin: 100px;">404 - Not Found</p>';
+      break;
+    default:
+    }
+  });
+
+  /////////////////////////////////////////////////////////////////////////
+  // Adapted from 'Single Page Apps for GitHub Pages'
+  // https://github.com/rafrex/spa-github-pages
+  // Copyright (c) 2016 Rafael Pedicini, licensed under the MIT License
+  // ----------------------------------------------------------------------
+  // Uses createBrowserHistory instead of window.history
+  /////////////////////////////////////////////////////////////////////////
+  (function(l) {
+    if (l.search) {
+      var q = {};
+      l.search.slice(1).split('&').forEach(function(v) {
+        var a = v.split('=');
+        q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+      });
+      if (q.p !== undefined) {
+        history.replace(
+          l.pathname.slice(0, -1) + (q.p || '') +
+            (q.q ? ('?' + q.q) : '') +
+            l.hash
+        );
+      }
+    }
+  }(window.location));
+
+
   //-------------------//
   // BUTTONS
   //-------------------//
