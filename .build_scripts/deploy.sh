515900c66d2abf34aac19bde57ffe355e47aa38d
Marc Farra <marcfarra@gmail.com>
2017-04-11 13:45:27 -0400

    Use ssh keys for deployment keys

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -4,10 +4,27 @@ set -e # halt script on error
 echo "Get ready, we're pushing to master!"
 cd _site
 echo "iranopendata.org" >> CNAME
+
+# Save some useful information
+SSH_REPO="git@github.com:${TRAVIS_REPO_SLUG}.git"
+SHA=`git rev-parse --verify HEAD`
+
+# Initialize repo
 git init
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
 git add .
-git commit -m "CI deploy to master"
+git commit -m "CI deploy to master ${SHA}"
 git show-ref
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:master
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git push --force --quiet $SSH_REPO master:master
